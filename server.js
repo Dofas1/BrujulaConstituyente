56627a1cd05216bd6273ad33faf8483ea53ae7d4
Dofas1 <b.ibarras@gmail.com>
2021-04-01 16:38:32 -0300

    Commit message

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-var http = require("http");
+const express = require('express');
+var morgan =require('morgan');
+const app=express();
+const func = require("./Funciones.js");
+app.use(express.json());
+app.use(morgan('dev'));
 
 function iniciar() {
-  function onRequest(request, response) {
-    console.log("Petición Recibida.");
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("Hola Mundo");
-    response.end();
-  }
-
-  http.createServer(onRequest).listen(8888);
-  console.log("Servidor Iniciado.");
+  app.listen(8888,()=>{
+    console.log("Servidor Iniciado.");
+  })
 }
 exports.iniciar = iniciar;
+app.get('/',(req,res)=>{
+  res.send("Hola ql");
+});
+app.post('/Procesa/:Respuestas',(req,res)=>{
+  var par_recibido=req.params.Respuestas;             //Parametro recibido en la solicitud
+  console.log(par_recibido);
+  func.ProcesaRespuestas(par_recibido).then( resultado=>{
+    console.log(resultado)
+    res.json(resultado);
+  });
+});
